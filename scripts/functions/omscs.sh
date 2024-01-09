a362122823f67f81621ad812c9bb4c2274e5989c
Aaron Miller <amiller719@gmail.com>
2024-01-09 17:48:06 -0500

    updating omscs function

--- scripts/functions/omscs.sh
+++ scripts/functions/omscs.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 omscs() {
-  cd "${CODE_DIR}/omscs/"
+  cd "${HOME}/omscs/"
 }
 
 add_notes() {
-  if [[ $PWD = "${CODE_DIR}/omscs/"* ]]; then
+  if [[ $PWD = "${HOME}/omscs/"* ]]; then
     git add .; git commit -m "adding notes."; git push
   else
     echo "not in omscs dir"
