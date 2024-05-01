1b307ab61c0123bb623858caa3ce386f0daf82bf
Aaron Miller <amiller719@gmail.com>
2024-05-01 00:26:01 -0400

    fixing install_symlinks func

--- scripts/install.sh
+++ scripts/install.sh
@@ -84,7 +84,7 @@ uninstall_dotfiles() {
 
 install_symlinks() {
   if [[ -x "$(command -v stow)" ]]; then
-    /bin/bash -c "$(stow -d "${DOTFILES_DIR}" . --ignore="\.alacritty.toml" --ignore="\.DS_Store" --ignore="\.git" --ignore="\.gitignore" --ignore="\.gitmodules")"
+    /bin/bash -c "$(stow -d "${DOTFILES_DIR}" . --ignore="\.DS_Store" --ignore="\.git" --ignore="\.gitignore" --ignore="\.gitmodules")"
   else
     echo -n "stow is not installed. Please install stow."
   fi
