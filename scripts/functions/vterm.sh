6010612d27596955b32a71a558b816839e6187cc
Aaron Miller <amiller719@gmail.com>
2024-05-05 20:08:01 -0400

    adding vterm_printf function

--- scripts/functions/vterm.sh
+++ scripts/functions/vterm.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+vterm_printf() {
+  if [ -n "$TMUX" ] && ([ "${TERM%%-*}" = "tmux" ] || [ "${TERM%%-*}" = "screen" ]); then
+    # Tell tmux to pass the escape sequences through
+    printf "\ePtmux;\e\e]%s\007\e\\" "$1"
+  elif [ "${TERM%%-*}" = "screen" ]; then
+    # GNU screen (screen, screen-256color, screen-256color-bce)
+    printf "\eP\e]%s\007\e\\" "$1"
+  else
+    printf "\e]%s\e\\" "$1"
+  fi
+}
+
 if [[ "$INSIDE_EMACS" = 'vterm' ]]; then
   unset ZLE_RPROMPT_INDENT
   alias clear='vterm_printf "51;Evterm-clear-scrollback";tput clear'
