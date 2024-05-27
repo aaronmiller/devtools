858c12bdb311d509770d4a847bd6c059afdc3876
Aaron Miller <amiller719@gmail.com>
2024-05-26 22:44:25 -0400

    cleaning up devtools

--- scripts/env/index.sh
+++ scripts/env/index.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-for file in "${DEVTOOLS_DIR}/scripts/env/"*; do
-  if [[ $file != *"index.sh" ]]; then
-    . $file
-  fi
-done
+if [[ $(uname -s) == "Darwin" ]]; then
+  for file in "${DEVTOOLS_DIR}/scripts/env/"*; do
+    if [[ $file != *"index.sh" ]]; then
+      . $file
+    fi
+  done
+fi
