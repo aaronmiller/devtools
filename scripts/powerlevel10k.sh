fb029e9683f615419dc1de721642a64eefd8ec92
Aaron Miller <amiller719@gmail.com>
2024-05-26 23:46:59 -0400

    fixing devtools

--- scripts/powerlevel10k.sh
+++ scripts/powerlevel10k.sh
@@ -3,7 +3,9 @@
 if [[ $(uname -s) == "Darwin" ]]; then
   # Powerlevel10k Theme
   . "$(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme"
-
-  # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-  [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+else
+  . /usr/share/powerlevel9k/powerlevel9k.zsh-theme
 fi
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
