d2192b6760f470afc2f0806324d3708714cc08cb
Aaron Miller <amiller719@gmail.com>
2024-05-28 10:05:05 -0400

    fixing devtools

--- scripts/ssh/ssh-agent.sh
+++ scripts/ssh/ssh-agent.sh
@@ -2,7 +2,7 @@
 
 if [[ $(uname -s) == "Linux" ]]; then
   if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-    eval $(ssh-agent)
+    eval "$(ssh-agent -s)"
     ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
   fi
 
