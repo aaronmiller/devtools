bd8f469bee78189ece75e8664571c39f68dafd99
Aaron Miller <amiller719@gmail.com>
2024-11-25 18:02:38 -0500

    updating doom_brighten_darken script

--- scripts/doom_brighten_darken.py
+++ scripts/doom_brighten_darken.py
@@ -16,9 +16,7 @@ def run(args):
     b_amount = args.b or 0
 
     if (r_amount or g_amount or b_amount) and amount:
-        logger.error(
-            "error: cannot set individual colors and total amount simultaneously. exiting..."
-        )
+        logger.error("error: cannot set individual colors and total amount simultaneously. exiting...")
         exit(1)
 
     if not (r_amount or g_amount or b_amount or amount):
@@ -95,9 +93,7 @@ def run(args):
                 new_secondary_b = hex(int_new_secondary_b)[2:]
 
             new_default_hex_color = f"{new_default_r}{new_default_g}{new_default_b}"
-            new_secondary_hex_color = (
-                f"{new_secondary_r}{new_secondary_g}{new_secondary_b}"
-            )
+            new_secondary_hex_color = f"{new_secondary_r}{new_secondary_g}{new_secondary_b}"
 
             lines.append(
                 [
@@ -176,27 +172,79 @@ def run(args):
             for line in alacritty_data:
                 f.write(line)
 
+    if args.k9s:
+        k9s_skin_file = Path(Path.home() / "devtools" / "kubernetes-devtools" / "k9s" / "skins" / "moonlight-wilmersdorf.yaml")
 
+        with k9s_skin_file.open() as f:
+            k9s_skin_data = f.readlines()
+
+        k9s_lines = []
+
+        for idx, line in enumerate(k9s_skin_data):
+            if ' "#' in line:
+                hash_idx = line.index(' "#')
+                hex_color = line[hash_idx + 3 : hash_idx + 9]
+
+                r = hex_color[0:2]
+                g = hex_color[2:4]
+                b = hex_color[4:6]
+
+                if op == "+":
+                    int_new_r = int(r, 16) + int(r_amount or amount)
+                    int_new_g = int(g, 16) + int(g_amount or amount)
+                    int_new_b = int(b, 16) + int(b_amount or amount)
+
+                elif op == "-":
+                    int_new_r = int(r, 16) - int(r_amount or amount)
+                    int_new_g = int(g, 16) - int(g_amount or amount)
+                    int_new_b = int(b, 16) - int(b_amount or amount)
+
+                if int_new_r < 16:
+                    new_r = zero_padding + hex(int_new_r)[2:]
+                else:
+                    new_r = hex(int_new_r)[2:]
+
+                if int_new_g < 16:
+                    new_g = zero_padding + hex(int_new_g)[2:]
+                else:
+                    new_g = hex(int_new_g)[2:]
+
+                if int_new_b < 16:
+                    new_b = zero_padding + hex(int_new_b)[2:]
+                else:
+                    new_b = hex(int_new_b)[2:]
+
+                new_hex_color = f"{new_r}{new_g}{new_b}"
+
+                k9s_lines.append(
+                    [
+                        idx,
+                        hex_color,
+                        new_hex_color,
+                    ]
+                )
+
+        for line in k9s_lines:
+            k9s_skin_data[line[0]] = k9s_skin_data[line[0]].replace(line[1], line[2])
+
+        with k9s_skin_file.open("w") as f:
+            for line in k9s_skin_data:
+                f.write(line)
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    parser = argparse.ArgumentParser(
-        prog="doombd", description="brighten or darken doom themes"
-    )
+    parser = argparse.ArgumentParser(prog="doombd", description="brighten or darken doom themes")
 
     parser.add_argument("-f", "--filename", help="file name of the doom theme")
-    parser.add_argument(
-        "-o", "--op", help="the operation to use, e.g., + or -"
-    )
-    parser.add_argument(
-        "--amount", help="the amount to increase or decrease for all colors"
-    )
+    parser.add_argument("-o", "--op", help="the operation to use, e.g., + or -")
+    parser.add_argument("--amount", help="the amount to increase or decrease for all colors")
     parser.add_argument("-r", help="the amount to increase or decrease for red")
     parser.add_argument("-g", help="the amount to increase or decrease for green")
     parser.add_argument("-b", help="the amount to increase or decrease for blue")
     parser.add_argument("--alacritty", action="store_true", help="set to enable changing colors for alacritty")
+    parser.add_argument("--k9s", action="store_true", help="set to enable changing colors for k9s skin")
 
     args = parser.parse_args()
 
