8819d913ac6a82d7141ceddbcd7fb05e92214ec4
Aaron Miller <amiller719@gmail.com>
2025-11-04 11:34:29 -0500

    updating python function

--- scripts/functions/python.sh
+++ scripts/functions/python.sh
@@ -3,9 +3,11 @@
 install_default_python_packages() {
   while read -r line; do pip install "${line}"; done <"${HOME}/.default-python-packages"
   while read -r line; do pip3 install "${line}"; done <"${HOME}/.default-python-packages"
+  asdf reshim
 }
 
 uninstall_default_python_packages() {
   pip freeze | while read -r line; do pip uninstall -y "${line}"; done
   pip3 freeze | while read -r line; do pip3 uninstall -y "${line}"; done
+  asdf reshim
 }
