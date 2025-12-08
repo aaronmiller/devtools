e99fbd481a9a86fa79365c9574c702974f592f61
Aaron Miller <amiller719@gmail.com>
2025-12-08 00:42:55 -0500

    updating doom_brighten_darken script

--- scripts/doom_brighten_darken.py
+++ scripts/doom_brighten_darken.py
@@ -15,7 +15,9 @@ def run(args):
     b_amount = args.b or 0
 
     if (r_amount or g_amount or b_amount) and amount:
-        logger.error("error: cannot set individual colors and total amount simultaneously. exiting...")
+        logger.error(
+            "error: cannot set individual colors and total amount simultaneously. exiting..."
+        )
         exit(1)
 
     if not (r_amount or g_amount or b_amount or amount):
@@ -31,7 +33,9 @@ def run(args):
         exit(1)
 
     if args.doom_theme:
-        doom_theme_file = Path(Path.home() / "dotfiles" / ".doom.d" / "themes" / f"{args.theme}-theme.el")
+        doom_theme_file = Path(
+            Path.home(), "dotfiles", ".doom.d", "themes", f"{args.theme}-theme.el"
+        )
 
         with doom_theme_file.open("r") as f:
             data = f.readlines()
@@ -45,7 +49,9 @@ def run(args):
                 secondary_hash_idx = line.index(' "#')
 
                 default_hex_color = line[default_hash_idx + 3 : default_hash_idx + 9]
-                secondary_hex_color = line[secondary_hash_idx + 3 : secondary_hash_idx + 9]
+                secondary_hex_color = line[
+                    secondary_hash_idx + 3 : secondary_hash_idx + 9
+                ]
 
                 default_r = default_hex_color[:2]
                 default_g = default_hex_color[2:4]
@@ -101,7 +107,9 @@ def run(args):
                     new_secondary_b = hex(int_new_secondary_b)[2:]
 
                 new_default_hex_color = f"{new_default_r}{new_default_g}{new_default_b}"
-                new_secondary_hex_color = f"{new_secondary_r}{new_secondary_g}{new_secondary_b}"
+                new_secondary_hex_color = (
+                    f"{new_secondary_r}{new_secondary_g}{new_secondary_b}"
+                )
 
                 doom_theme_lines.append(
                     [
@@ -122,7 +130,7 @@ def run(args):
                 f.write(line)
 
     if args.alacritty:
-        alacritty_file = Path(Path.home() / "dotfiles" / f"alacritty-{args.theme}.toml")
+        alacritty_file = Path(Path.home(), "dotfiles", f"alacritty-{args.theme}.toml")
 
         with alacritty_file.open() as f:
             alacritty_data = f.readlines()
@@ -182,7 +190,14 @@ def run(args):
                 f.write(line)
 
     if args.k9s:
-        k9s_skin_file = Path(Path.home() / "devtools" / "kubernetes-devtools" / "k9s" / "skins" / f"{args.theme}.yaml")
+        k9s_skin_file = Path(
+            Path.home(),
+            "devtools",
+            "kubernetes-devtools",
+            "k9s",
+            "skins",
+            f"{args.theme}.yaml",
+        )
 
         with k9s_skin_file.open() as f:
             k9s_skin_data = f.readlines()
@@ -245,17 +260,33 @@ def run(args):
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    parser = argparse.ArgumentParser(prog="doombd", description="brighten or darken doom themes")
+    parser = argparse.ArgumentParser(
+        prog="doombd", description="brighten or darken doom themes"
+    )
 
     parser.add_argument("-o", "--op", help="the operation to use, e.g., + or -")
-    parser.add_argument("--amount", help="the amount to increase or decrease for all colors")
+    parser.add_argument(
+        "--amount", help="the amount to increase or decrease for all colors"
+    )
     parser.add_argument("-r", help="the amount to increase or decrease for red")
     parser.add_argument("-g", help="the amount to increase or decrease for green")
     parser.add_argument("-b", help="the amount to increase or decrease for blue")
-    parser.add_argument("--doom-theme", action="store_true", help="set to enable changing colors for selected doom theme")
-    parser.add_argument("--theme", help="set to enable changing colors for selected theme")
-    parser.add_argument("--alacritty", action="store_true", help="set to enable changing colors for alacritty")
-    parser.add_argument("--k9s", action="store_true", help="set to enable changing colors for k9s skin")
+    parser.add_argument(
+        "--doom-theme",
+        action="store_true",
+        help="set to enable changing colors for selected doom theme",
+    )
+    parser.add_argument(
+        "--theme", help="set to enable changing colors for selected theme"
+    )
+    parser.add_argument(
+        "--alacritty",
+        action="store_true",
+        help="set to enable changing colors for alacritty",
+    )
+    parser.add_argument(
+        "--k9s", action="store_true", help="set to enable changing colors for k9s skin"
+    )
 
     args = parser.parse_args()
 
