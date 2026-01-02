c0ada927cc7fb48cea003908a2c425bc13b9e2b7
Aaron Miller <amiller719@gmail.com>
2026-01-02 13:41:35 -0500

    using pipx instead of pip3

--- scripts/functions/python.sh
+++ scripts/functions/python.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 install_default_python_packages() {
-  while read -r line; do pip3 install "${line}"; done <"${HOME}/.default-python-packages"
-  asdf reshim
+  while read -r line; do pipx install "${line}"; done <"${HOME}/.default-python-packages"
 }
 
 uninstall_default_python_packages() {
-  pip3 freeze | while read -r line; do pip3 uninstall -y "${line}"; done
-  asdf reshim
+  pipx uninstall-all
 }
