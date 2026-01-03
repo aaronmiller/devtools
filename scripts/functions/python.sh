b7a36d12c02f72a7f03e8c853fef9e3712cd1b32
Aaron Miller <amiller719@gmail.com>
2026-01-02 19:29:40 -0500

    updating default python packages file reference

--- scripts/functions/python.sh
+++ scripts/functions/python.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 install_default_python_packages() {
-  while read -r line; do pipx install "${line}"; done <"${HOME}/.default-python-packages"
+  while read -r line; do pipx install "${line}"; done <"${HOME}/.requirements.txt"
 }
 
 uninstall_default_python_packages() {
