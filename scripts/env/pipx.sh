6a32ef37f03a2c18d00edaf136df36b4e045f8f4
Aaron Miller <amiller719@gmail.com>
2026-01-02 21:06:06 -0500

    fixing PIPX_DEFAULT_PYTHON environment variable

--- scripts/env/pipx.sh
+++ scripts/env/pipx.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export PIPX_BIN_DIR="${HOME}/bin"
-export PIPX_DEFAULT_PYTHON=$(asdf which python3)
+export PIPX_DEFAULT_PYTHON="${HOME}/.asdf/installs/python/$(cat < "${HOME}/.tool-versions" | grep python | cut -d " " -f 3)/bin/python3"
