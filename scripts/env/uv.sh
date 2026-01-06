eed2dbb79f109fb5a0956d5f3b83c42a0efa5a68
Aaron Miller <amiller719@gmail.com>
2026-01-06 16:40:41 -0500

    fixing local bin path

--- scripts/env/uv.sh
+++ scripts/env/uv.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-export UV_TOOL_BIN_DIR="${HOME}/.uv/tools/bin"
-export UV_TOOL_DIR="${HOME}/.uv/tools"
 export UV_PYTHON="${HOME}/.asdf/installs/python/$(cat < "${HOME}/.tool-versions" | grep python | cut -d " " -f 3)/bin/python3"
