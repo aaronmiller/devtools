531f022b2a6f2c87afbd205b5233287b868ec7fb
Aaron Miller <amiller719@gmail.com>
2026-01-06 16:25:57 -0500

    removing pipx and using uv

--- scripts/env/pipx.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export PIPX_BIN_DIR="${HOME}/bin"
-export PIPX_DEFAULT_PYTHON="${HOME}/.asdf/installs/python/$(cat < "${HOME}/.tool-versions" | grep python | cut -d " " -f 3)/bin/python3"
--- scripts/functions/python.sh
+++ scripts/functions/python.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-install_default_python_packages() {
-  while read -r line; do pipx install "${line}"; done <"${HOME}/.requirements.txt"
+install_default_uv_tools() {
+  while read -r line; do uv tool install "${line}"; done <"${HOME}/.requirements.txt"
 }
 
-uninstall_default_python_packages() {
-  pipx uninstall-all
+uninstall_default_uv_tools() {
+  uv tool uninstall --all
 }
