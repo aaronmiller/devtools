18e70b8aeaae42b4e9ac7b51ec6c5835226c51bc
Aaron Miller <amiller719@gmail.com>
2026-04-15 12:31:45 -0400

    cleaning up devtools

---
+++ scripts/env/path.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export PATH="${DEVTOOLS_DIR}/emacs/nextstep/Emacs.app/Contents/MacOS:${PATH}"
+export PATH="${DEVTOOLS_DIR}/emacs/nextstep/Emacs.app/Contents/MacOS/bin:${PATH}"
